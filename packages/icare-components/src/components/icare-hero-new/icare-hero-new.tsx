b188c3b299ec23ac26ff8d26f5f8c052d14d73d3
KasiaRaven26 <kasiaraven2507@gmail.com>
2025-10-03 21:01:04 +0200

    commitedonFriday

--- packages/icare-components/src/components/icare-hero-new/icare-hero-new.tsx
+++ packages/icare-components/src/components/icare-hero-new/icare-hero-new.tsx
@@ -1,41 +1,41 @@
 import { Component, Host, h, Prop } from "@stencil/core";
 
 @Component({
-  tag: "icare-hero-new",
-  styleUrl: "icare-hero-new.scss",
-  shadow: true
+    tag: "icare-hero-new",
+    styleUrl: "icare-hero-new.scss",
+    shadow: true
 })
 
 export class IcareHeroNew {
 
-  @Prop() imageSrc = "images/heros/hero-landing-page.jpg";
-  @Prop() imgAlt = "iCare hero image";
+    @Prop() imageSrc = "images/heros/hero-landing-page.jpg";
+    @Prop() imgAlt = "iCare hero image";
 
-  render() {
-    return (
-      <Host>
-        <section aria-label="Caregivers hero">
-          <img src={this.imageSrc} alt="Care coordination background" />
-          <div class='hero-overlay' />
-          <div class='icare-header' >
-            <icare-header>
-              <slot name="nav-links" slot="nav-links" />
-              <slot name="header-buttons" slot="header-buttons" />
-            </icare-header>
-          </div>
-          <div class='content'>
-            <h1><slot name='header-content' /></h1>
-            <h2>
-              <slot name='subheader-content' />
-            </h2>
-            <div class='cta'>
-              <slot name='cta-buttons' />
-              {/* <icare-button variant='primary'>Get Started</icare-button>
+    render() {
+        return (
+            <Host>
+                <section aria-label="Caregivers hero">
+                    <img src={this.imageSrc} alt="Care coordination background" />
+                    <div class='hero-overlay' />
+                    <div class='icare-header' >
+                        <icare-header>
+                            <slot name="nav-links" slot="nav-links" />
+                            <slot name="header-buttons" slot="header-buttons" />
+                        </icare-header>
+                    </div>
+                    <div class='content'>
+                        <h1><slot name='header-content' /></h1>
+                        <h2>
+                            <slot name='subheader-content' />
+                        </h2>
+                        <div class='cta'>
+                            <slot name='cta-buttons' />
+                            {/* <icare-button variant='primary'>Get Started</icare-button>
               <icare-button variant='secondary'>More Information</icare-button> */}
-            </div>
-          </div>
-        </section>
-      </Host>
-    );
-  }
+                        </div>
+                    </div>
+                </section>
+            </Host>
+        );
+    }
 }
