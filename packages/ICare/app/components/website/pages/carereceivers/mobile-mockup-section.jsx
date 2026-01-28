8f7fca4b536eb36fc70b3e69e4accbc84c65ce0d
Max Herbst <mxh1985@gmail.com>
2026-01-28 20:09:40 +0100

    commit

--- packages/ICare/app/components/website/pages/carereceivers/mobile-mockup-section.jsx
+++ packages/ICare/app/components/website/pages/carereceivers/mobile-mockup-section.jsx
@@ -13,7 +13,7 @@ export default function MobileMockupSection() {
                 unstyledImage
             >
 
-                <h2 className={styles.myicare}><span>My</span><img src="/images/logo/icareblack.svg" alt="ICare" /><span>App</span>
+                <h2 className={styles.myicare}><span>My</span><img src="/images/logo/icareblack.svg" alt="ICare" /><span>app</span>
                 </h2>
 
 
