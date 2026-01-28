0909306003107e528943fb8800591816d9e851b4
ajkirwan1 <ajkirwan1@gmail.com>
2026-01-28 19:35:07 +0000

    Favicon (#53)
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: Max Herbst <mxh1985@gmail.com>

--- packages/ICare/app/components/website/pages/how-it-works/CompareAgencyVsICare.jsx
+++ packages/ICare/app/components/website/pages/how-it-works/CompareAgencyVsICare.jsx
@@ -178,7 +178,7 @@ export default function CompareAgencyVsICare() {
                                 }}
                             >
                                 <img
-                                    src="/images/logo/icareblack.svg"
+                                    src="/images/logo/icare-hub-black.svg"
                                     alt="ICare"
                                     style={{ height: 35, width: "auto", display: "block" }}
                                 />
