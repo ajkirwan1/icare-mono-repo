9620ee570de1dbb0a9a6bdcc0a1fda866b4a36e6
ajkirwan1 <ajkirwan1@gmail.com>
2026-02-05 21:38:47 +0000

    commit (#78)
    Co-authored-by: KasiaRaven26 <kasiaraven2507@gmail.com>

--- packages/ICare/app/components/website/pages/shared/home-page-hero.jsx
+++ packages/ICare/app/components/website/pages/shared/home-page-hero.jsx
@@ -34,7 +34,7 @@ export default function HomePageHero({ imgSrc }) {
 
                             {/* HEADLINE */}
                             <h1 className={styles.heading}>
-                                Direct home care without agency barriers
+                                Connect with independent carers directly
 
                             </h1>
 
